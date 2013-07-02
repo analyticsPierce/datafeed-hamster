a1b1e8a550ea59237671b3d54030cc721b49d0b8
analyticsPierce <jason@marketingscience.co>
2013-07-01 23:19:11 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DatafeedHamster::Application.config.secret_key_base = 'b6c892eec190326bce5dc6d921ce519f845d45625baa4830324e2f49e8d0f9e7d4cbee58b4eb7ab6d75c29a611a96a5320a998a79adc03ff1495646812db0b14'
